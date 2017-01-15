27d049fbfc9f57db67d234cb29bb3d21ad2de579
wangruwei <wangruwei@hotmail.com>
2017-01-15 09:09:06 +0800

    add gulp rev

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@ import sass       from 'gulp-ruby-sass';
 import sourcemaps from 'gulp-sourcemaps';
 import path       from 'path';
 import md5        from 'gulp-md5-plus';
+import rev		  from 'gulp-rev';
 
 let _config = {
 	sassRoot : path.join(__dirname, 'public/css/'),
@@ -24,6 +25,14 @@ gulp.task('md5', () => {
 	return gulp.src(`${_config.sassRoot}*.css`)
 		.pipe(md5(10, `${_config.ejsRoot}**/*.ejs`))
 		.pipe(gulp.dest(_config.sassRoot));
+});
+
+gulp.task('rev', () => {
+	return gulp.src('public/css/**/*.css')
+		.pipe(rev())
+		.pipe(gulp.dest('public/css'))
+		.pipe(rev.manifest())
+		.pipe(gulp.dest('public/css'));
 });
 
 gulp.task('watch', ['sass'], () => {
