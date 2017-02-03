5d4de910e2ad07aada0d4c00a535af7db6d2329a
wangruwei <wangruwei@hotmail.com>
2017-02-03 17:55:31 +0800

    getJS

--- public/js/tools/Tools.js
+++ public/js/tools/Tools.js
@@ -1,17 +1,16 @@
 define(['jquery', 'semantic'], function($){
-	var appUrl = 'public/js/apps/';
-	var test = params.test || false;
-	console.log('test: ', test);
+	var appUrl  = 'public/js/apps/';
+	var modules = modules || {};
+
 	return {
 		getJS   : function(appName, test){
 			return function(fileName){
-				return test ? 'apps/' + appName + '/modules/' + fileName
-					        : 'apps/' + appName + '/modules/' + fileName;
+				return test ? '/js/apps/' + appName + '/modules/' + fileName + '.js'
+			        	    : modules['js/apps/' + appName + '/modules/' + fileName + '.js'];
 			};
 		},
 		getTemp : function(appName, test){
 			return function(fileName){
-				// console.log(modules['js/apps/' + appName + '/templates/' + fileName + '.html']);
 				return test ? 'js/apps/' + appName + '/templates/' + fileName
 					        : 'js/apps/' + appName + '/templates/' + fileName;
 			};
